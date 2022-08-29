319276089d2d4208ba3698cc94a9d132c3fc0304
DustoTail <dustotail@gmail.com>
2022-08-29 15:13:39 +0900

    modified premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,8 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -24,9 +26,11 @@ project "ImGui"
 
     filter "system:windows"
         systemversion "latest"
-        cppdialect "C++20"
-        staticruntime "On"
 
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
 
-    filter {"system:windows", "configurations:Release"}
-        buildoptions "/MT"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
